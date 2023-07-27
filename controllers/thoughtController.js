e91a259e2281817648782a34b6162e1aabd1b20a
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-07-26 21:10:23 -0400

    add code to thoughtRoutes.js file to define what each route does in the controller, debug thoughtControllers file and delete an unnecessary line of code in userControllers file.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -30,7 +30,7 @@ module.exports = {
         try {
             const thought = await Thought.create(req.body);
             const user = await User.findOneAndUpdate(
-                {_id: req.params.userId},
+                {_id: req.body.userId},
                 {$addToSet: {thoughts: thought._id}},
                 {new: true}
             )
