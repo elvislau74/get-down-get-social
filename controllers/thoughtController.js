af6ef2698819c21005335589256c88f0b682f3fb
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-07-29 23:22:55 -0400

    fix bugs in controllers, models and routes folders

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -115,7 +115,7 @@ module.exports = {
         try {
             const thought = await Thought.findOneAndUpdate(
                 {_id: req.params.thoughtId},
-                {$pull: {reactions: {reactionId: req.params.reactionId}}},
+                {$pull: {reactions: {_id: req.params.reactionId}}},
                 {runValidators: true, new: true}
             );
 
