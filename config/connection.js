6f078b9a33e79ce5dacbda659a5a18d9d5ef2ff2
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-07-30 23:15:41 -0400

    add comments to config folder and index.js file

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
+// call required dependencies with mongoose
 const { connect, connection } = require('mongoose');
 
+// Wrap Mongoose around local connection to MongoDB
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialMediaDB';
 
 connect(connectionString);
 
+// Export the connection
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+// call all necessary dependencies and files
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const cwd = process.cwd();
 
+// Port number
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('01-Activities')[1]
+const activity = cwd.includes('Get-Down-Get-Social')
+  ? cwd.split('Get-Down-Get-Social')[1]
   : cwd;
 
+// Use all middleware and routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+// run the server
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for ${activity} running on port ${PORT}!`);
